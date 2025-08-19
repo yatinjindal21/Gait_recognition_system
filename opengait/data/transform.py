4c37751d964cab32845c1bf677bf97982caf6138
Yatin Jindal <jindalyatin2109@gmail.com>
2025-08-19 15:04:12 +0530

    added data folder

---
+++ opengait/data/transform.py
@@ -0,0 +1,34 @@
+import numpy as np
+import random
+import torchvision.transforms as T
+import math
+from data import transform as base_transform
+from utils import is_list, is_dict, get_valid_args
+
+
+class BaseSilCuttingTransform():
+    def __init__(self, divsor=255.0, cutting=None):
+        self.divsor = divsor
+        self.cutting = cutting
+
+    def __call__(self, x):
+        if self.cutting is not None:
+            cutting = self.cutting
+        else:
+            cutting = int(x.shape[-1] // 64) * 10
+        if cutting != 0: 
+            x = x[..., cutting:-cutting]
+        return x / self.divsor
+
+
+def get_transform(trf_cfg=None):
+    if is_dict(trf_cfg):
+        transform = getattr(base_transform, trf_cfg['type'])
+        valid_trf_arg = get_valid_args(transform, trf_cfg, ['type'])
+        return transform(**valid_trf_arg)
+    if trf_cfg is None:
+        return lambda x: x
+    if is_list(trf_cfg):
+        transform = [get_transform(cfg) for cfg in trf_cfg]
+        return transform
+    raise "Error type for -Transform-Cfg-"
